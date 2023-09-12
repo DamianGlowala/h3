8cfe9d12cf413d1f8370ad36c9af64f515eaae01
Estéban <esteban.soubiran@insa-cvl.fr>
2023-09-12 23:18:39 +0200

    docs: fix comments

--- examples/hooks.ts
+++ examples/hooks.ts
@@ -8,46 +8,25 @@ import {
   toNodeListener,
 } from "h3";
 
-// const app = createApp()
+const app = createApp()
 
-// const router = createRouter()
-//   .get('/', defineEventHandler({
-//     onRequest: defineRequestMiddleware(() => {
-//       // Do anything you want here like authentication, rate limiting, etc.
-//       console.log('onRequest')
-//       // Never return anything from onRequest to avoid to close the connection
-//     }),
-//     handler: defineEventHandler(() => {
-//       return "GET: hello world"
-//     }),
-//     onBeforeResponse: defineResponseMiddleware(() => {
-//       // Do anything you want here like logging, collecting metrics, or output compression, etc.
-//       console.log('onResponse')
-//       // Never return anything from onResponse to avoid to close the connection
-//     })
-//   }))
-
-// app.use(router)
-
-// createServer(toNodeListener(app)).listen(3000)
-
-const app = createApp();
-
-const router = createRouter().get(
-  "/",
-  defineEventHandler({
+const router = createRouter()
+  .get('/', defineEventHandler({
     onRequest: defineRequestMiddleware(() => {
-      console.log("onRequest");
+      // Do anything you want here like authentication, rate limiting, etc.
+      console.log('onRequest')
+      // Never return anything from onRequest to avoid to close the connection
     }),
     handler: defineEventHandler(() => {
-      return "GET: hello world";
+      return "GET: hello world"
     }),
     onBeforeResponse: defineResponseMiddleware(() => {
-      console.log("onResponse");
-    }),
-  }),
-);
+      // Do anything you want here like logging, collecting metrics, or output compression, etc.
+      console.log('onResponse')
+      // Never return anything from onResponse to avoid to close the connection
+    })
+  }))
 
-app.use(router);
+app.use(router)
 
-createServer(toNodeListener(app)).listen(3000);
+createServer(toNodeListener(app)).listen(3000)
